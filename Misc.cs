77753d7c12fd5c0b6bb7cad69a1167b455102a1b
AmphibianDev <110111354+AmphibianDev@users.noreply.github.com>
2022-09-24 19:08:01 +0300

    Adding my old project

---
+++ Misc.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace StockViewer
+{
+    class Misc
+    {
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern UInt32 GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        public static void Unclikcable(IntPtr handle)
+        {
+            SetWindowLong(handle, -20, (IntPtr)(Misc.GetWindowLong(handle, -20) | 0x80000 | 0x20));
+        }
+
+        public static bool ContrastColor(Color color)
+        {
+            double luminance = (0.299 * color.R + 0.587 * color.G + 0.114 * color.B) / 255;
+
+            if (luminance > 0.6)
+                return false;
+            else
+                return true;
+        }
+
+        public static Color GetColorAt(Point location)
+        {
+            Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+            using (Graphics gdest = Graphics.FromImage(screenPixel))
+            {
+                using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    IntPtr hSrcDC = gsrc.GetHdc();
+                    IntPtr hDC = gdest.GetHdc();
+                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
+                    gdest.ReleaseHdc();
+                    gsrc.ReleaseHdc();
+                }
+            }
+
+            return screenPixel.GetPixel(0, 0);
+        }
+    }
+}
---
+++ TextData.cs
@@ -0,0 +1,80 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+namespace StockViewer
+{
+    class TextData
+    {
+        public RichTextBox TextBox { get { return textBox; } }
+        public float CordX { get { return cordX; } set {
+                cordX = value;
+                textBox.Location = new Point((int)cordX, textBox.Location.Y);
+            } }
+
+        RichTextBox textBox;
+        float cordX = 0;
+
+        public bool white;
+
+        public TextData(RichTextBox textBox)
+        {
+            this.textBox = textBox;
+        }
+
+        public void Set_FontSize(int fontSize)
+        {
+            //Serif
+            textBox.Font = new Font("Tw Cen MT", fontSize, FontStyle.Bold);
+        }
+
+        public void Set_Text(string text)
+        {
+            textBox.Text = text;
+            Color_ProcentText();
+        }
+
+        public void Update_Poz(float movment)
+        {
+            cordX += movment;
+            textBox.Location = new Point((int)cordX, textBox.Location.Y);
+        }
+
+        public void Update_Color(Color backColor)
+        {
+            white = !white;
+            if (white)
+            {
+                textBox.BackColor = backColor;
+                textBox.ForeColor = Color.White;
+            }
+            else
+            {
+                textBox.BackColor = backColor;
+                textBox.ForeColor = Color.Black;
+            }
+            Color_ProcentText();
+        }
+
+        void Color_ProcentText()
+        {
+            var words = textBox.Text.Split();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > 0 && words[i][0] == '(')
+                {
+                    if (words[i][1] == '-')
+                    {
+                        textBox.Select(textBox.Text.IndexOf(words[i]), words[i].Length);
+                        textBox.SelectionColor = Color.Red;
+                    }
+                    else
+                    {
+                        textBox.Select(textBox.Text.IndexOf(words[i]), words[i].Length);
+                        textBox.SelectionColor = Color.ForestGreen;
+                    }
+                }
+            }
+        }
+    }
+}
